d1e2123472c45c7cf5a1fdcf8ac2c4497935e2ab
Kasperi Palkama <kasperi.palkama@gmail.com>
2017-04-03 19:56:51 +0300

    add assignment3

---
+++ Assignment3/PCA.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 17:54:09 2017
+
+@author: kasperipalkama
+"""
+
+from sklearn.decomposition import PCA
+import pandas as pd
+from matplotlib import style,pyplot as plt
+import numpy as np
+
+def getData(path):
+    return pd.read_table(path,sep = ',',header = None)    
+
+def dimReduction(data):
+    pca = PCA(n_components=2).fit(data)
+    return pca.transform(data)
+
+def scatterPlot(data,party,label,xlabel,ylabel):
+    style.use('ggplot')
+    plt.figure()
+    l1 = plt.plot(data[np.where(party == 0)[0],0],
+             data[np.where(party == 0)[0],1],'bo',label = label[0])
+    l2 = plt.plot(data[np.where(party == 1)[0],0],
+             data[np.where(party == 1)[0],1],'ro',label = label[1])
+    plt.legend(loc = 'best')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig('congress_votes.png')
+
+if __name__ == '__main__':
+    votes = getData('input data/congress_votes.dat')
+    party = getData('input data/congress_party.dat')
+    dim_red_votes = dimReduction(votes)
+    scatterPlot(data = dim_red_votes,party = party,
+                label = ['democrat', 'rebublican'],
+                xlabel='PC1', ylabel='PC2')
+    
